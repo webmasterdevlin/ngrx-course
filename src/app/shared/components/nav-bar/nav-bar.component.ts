aba638c8c33f6f44a8e9c9ebd28b5778aa41d63f
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-02-13 23:10:41 +0100

    updated

--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -3,6 +3,7 @@ import { Store } from "@ngrx/store";
 import { selectHeroesState, selectVillainsState, State } from "src/app/store";
 import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy";
 import { loadHeroes } from "src/app/store/actions/hero.actions";
+import { loadVillains } from "src/app/store/actions/villain.actions";
 
 @UntilDestroy()
 @Component({
@@ -20,8 +21,9 @@ export class NavBarComponent implements OnInit {
     this.getStore();
   }
 
-  handleLoadHeroes() {
+  handleLoadCharacters() {
     this.store.dispatch(loadHeroes());
+    this.store.dispatch(loadVillains());
   }
 
   private getStore() {
