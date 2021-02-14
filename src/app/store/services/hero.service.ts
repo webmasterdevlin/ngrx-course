2e795ae5b365279440d9f1b5828d48727b4f6fdf
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-02-14 15:16:40 +0100

    updated

--- src/app/store/services/hero.service.ts
+++ src/app/store/services/hero.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from "@angular/core";
 import { environment } from "../../../environments/environment";
 import { HttpClient } from "@angular/common/http";
-import { Observable, throwError } from "rxjs";
+import { Observable } from "rxjs";
 import { Hero } from "../../features/hero/hero.model";
-import { catchError } from "rxjs/operators";
 
 @Injectable()
 export class HeroService {
@@ -12,26 +11,18 @@ export class HeroService {
   constructor(private http: HttpClient) {}
 
   getHeroes(): Observable<Hero[]> {
-    return this.http
-      .get<Hero[]>(this.path)
-      .pipe(catchError((err) => throwError(err.message)));
+    return this.http.get<Hero[]>(this.path);
   }
 
   deleteHero(id: string): Observable<void> {
-    return this.http
-      .delete<void>(`${this.path}/${id}`)
-      .pipe(catchError((err) => throwError(err.message)));
+    return this.http.delete<void>(`${this.path}/${id}gjgjhg`);
   }
 
   postHero(createdHero: Hero): Observable<Hero> {
-    return this.http
-      .post<Hero>(this.path, createdHero)
-      .pipe(catchError((err) => throwError(err.message)));
+    return this.http.post<Hero>(this.path, createdHero);
   }
 
   putHero(updatedHero: Hero): Observable<void> {
-    return this.http
-      .put<void>(`${this.path}/${updatedHero.id}`, updatedHero)
-      .pipe(catchError((err) => throwError(err.message)));
+    return this.http.put<void>(`${this.path}/${updatedHero.id}`, updatedHero);
   }
 }
--- src/app/store/services/villain.service.ts
+++ src/app/store/services/villain.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from "@angular/core";
 import { environment } from "../../../environments/environment";
 import { HttpClient } from "@angular/common/http";
-import { Observable, throwError } from "rxjs";
+import { Observable } from "rxjs";
 import { Villain } from "../../features/villain/villain.model";
-import { catchError } from "rxjs/operators";
 
 @Injectable()
 export class VillainService {
@@ -12,26 +11,21 @@ export class VillainService {
   constructor(private http: HttpClient) {}
 
   getVillains(): Observable<Villain[]> {
-    return this.http
-      .get<Villain[]>(this.path)
-      .pipe(catchError((err) => throwError(err.message)));
+    return this.http.get<Villain[]>(this.path);
   }
 
   deleteVillain(id: string): Observable<void> {
-    return this.http
-      .delete<void>(`${this.path}/${id}`)
-      .pipe(catchError((err) => throwError(err.message)));
+    return this.http.delete<void>(`${this.path}/${id}`);
   }
 
   postVillain(createdVillain: Villain): Observable<Villain> {
-    return this.http
-      .post<Villain>(this.path, createdVillain)
-      .pipe(catchError((err) => throwError(err.message)));
+    return this.http.post<Villain>(this.path, createdVillain);
   }
 
   putVillain(updatedVillain: Villain): Observable<void> {
-    return this.http
-      .put<void>(`${this.path}/${updatedVillain.id}`, updatedVillain)
-      .pipe(catchError((err) => throwError(err.message)));
+    return this.http.put<void>(
+      `${this.path}/${updatedVillain.id}`,
+      updatedVillain
+    );
   }
 }
