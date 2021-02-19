65a4020a46635a91850d2f657cc48e6a193e3333
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-02-19 19:33:17 +0100

    updated

--- src/app/store/services/hero.service.ts
+++ src/app/store/services/hero.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { environment } from "../../../environments/environment";
+import { environment } from "src/environments/environment";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { Hero } from "../../features/hero/hero.model";
